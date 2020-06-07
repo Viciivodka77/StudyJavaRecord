8d68d997cdbaca2773a25aedf9c1e560e4cdc118
Viciivodka77 <Viciivodka77@outlook.com>
2020-06-07 20:55:59 +0800

    Study java record

---
+++ src/StudyDay25/FindLadders.java
@@ -0,0 +1,9 @@
+package StudyDay25;
+
+import java.util.List;
+
+public class FindLadders {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        
+    }
+}
