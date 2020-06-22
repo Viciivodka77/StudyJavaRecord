347893cdc3073fb20b76fb5ffc055cb432cfe908
Viciivodka77 <Viciivodka77@outlook.com>
2020-06-22 18:37:46 +0800

    Study java record

---
+++ src/StudyDay39/PatternMatching.java
@@ -0,0 +1,75 @@
+package StudyDay39;
+
+public class PatternMatching {
+    public boolean patternMatching(String pattern, String value) {
+        //字符长度为0
+        if (value.length() == 0) {
+            //匹配串长度不为 0
+            if (pattern.length() != 0) {
+                for (int i = 1; i < pattern.length(); i++) {
+                    //判断是否匹配
+                    if (pattern.charAt(i - 1) != pattern.charAt(i)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        if (pattern.length() == 0) return false;
+        if (pattern.length() == 1) return true;
+
+        char a =pattern.charAt(0);
+        //计算出pattern中a和b的个数
+        int countA = 0,countB = 0;
+        for (int i = 0; i < pattern.length(); i++) {
+            if (a == pattern.charAt(i)){
+                countA++;
+            }else{
+                countB++;
+            }
+        }
+        int lenP = pattern.length(),lenV = value.length();
+        //pattern中只有a，没有b   ->    countA * lenA 应该等于 lenV
+        if (countB == 0){
+            if (lenV % countA != 0) return false;
+            int lenA = lenV / countA;
+            String valueA = value.substring(0,lenA);
+            for (int i = valueA.length(); i < lenV; i += valueA.length()) {
+                if (!value.substring(i, i + lenA).equals(valueA)) return false;
+            }
+            return true;
+        }
+        //countA * lenA  + countB * lenB = lenV
+        for (int lenA = 0; lenA * countA <= lenV ; lenA++) {
+            int temp = lenV - countA *lenA;
+            //如果当前lenA的假设无法满足 则跳过当前假设
+            if (temp % countB != 0 ) continue;
+            int lenB = temp / countB,ci = 0;
+            boolean isMatch = true;
+            String valueA = "",valueB = "";
+            for (char c : pattern.toCharArray()) {
+                if (c == a) {
+                    String cur_a = value.substring(ci,ci += lenA);
+                    if (valueA.length() == 0 ){
+                        valueA = cur_a;
+                    }else if (!valueA.equals(cur_a)){
+                        isMatch = false;
+                        break;
+                    }
+                }else {
+                    String cur_b = value.substring(ci , ci += lenB);
+                    if (valueB.length() == 0){
+                        valueB = cur_b;
+                    }else if (!valueB.equals(cur_b)){
+                        isMatch = false;
+                        break;
+                    }
+                }
+            }
+            if (isMatch && !valueA.equals(valueB)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
